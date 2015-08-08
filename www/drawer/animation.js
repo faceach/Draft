22867a339d867175838a4602b76845496c7cfb22
faceach <faceach@gmail.com>
2015-08-08 10:28:34 +0800

    notransition

--- www/drawer/animation.js
+++ www/drawer/animation.js
@@ -1,8 +1,10 @@
     var toggleClass = "b_hide";
     var notransitionClass = "notransition";
-    function sj_be(el, event, callback){
-    	el.addEventListener(event, callback, false);
+
+    function sj_be(el, event, callback) {
+        el.addEventListener(event, callback, false);
     }
+
     function bindGlobalDrawer(drawer) {
         var actContainer = drawer.querySelector(".actContainer");
 
@@ -38,6 +40,7 @@
         var tmpHeight = ele.offsetHeight;
         ele.classList.toggle(anim);
     }
+
     function setHeight(ele) {
         var height = ele.clientHeight;
         if (height == 0 && ele.classList.contains(toggleClass)) {
@@ -48,18 +51,22 @@
         if (height > 0) {
             ele.style.height = height + "px";
         }
+        if (ele.classList.contains(notransitionClass)) {
+            // Recover CSS transition
+            ele.classList.remove(notransitionClass);
+        }
     }
+
     function transitionEndHandler(evt, ele, lastProperty) {
         if (evt.target != ele) {
             return;
         }
         if (evt.propertyName === "height") {
-            // ...
+            // Disable CSS transition before remove "height" inline sytle,
+            // As this will trigger strange animation on Safari-IOS: [height] - [0] - [height] 
             ele.classList.add(notransitionClass);
             // upon height finishes animating, it should be cleared out.
             ele.style.removeProperty('height');
-            // ...
-            ele.classList.remove(notransitionClass);
         }
         // send an event to signal that the ele has finished all transitions
         var prop = ele.classList.contains("b_hide") ? lastProperty[1] : lastProperty[0];
