5a689f99a0f25f6e57073667f1564d1c92748d99
Xiaomin Liu <xiaoml@microsoft.com>
2015-11-11 19:39:01 +0800

    sendaction

---
+++ www/proactive/sendAction.js
@@ -0,0 +1,15 @@
+{
+	"Uri": "action://CuOutput",
+	"SystemAction": {
+		"ReachedConfirmationTurn": false,
+		"Uri": "action://Email/SendEmail",
+		"Status": "InProgress",
+		"StatusInfo": "",
+		"TaskFrameId": "dd378949-62d2-4695-ada7-d5e9e1c2e598",
+		"TurnIndex": 1
+	},
+	"ConversationId": "9e411f595121ca4888e378d4eacfe977",
+	"TraceId": "17A90119B4014A4C94E15E59844AA35E",
+	"ImpressionId": "267eda9dd649ca458e6621a015a01abf",
+	"LgObject": null
+}
