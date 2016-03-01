8eee9813b5b9cbbe0032a0179abdb4fc3e2a8169
Xiaomin Liu <xiaoml@microsoft.com>
2016-03-01 19:31:44 +0800

    actions

---
+++ www/Actions/reminder_time_recurrence.js
@@ -0,0 +1,33 @@
+{
+        ConversationId = "82c4606e-2288-929c-9e63-a4b0607e8d60";
+        ImpressionId = 852f0969d8dd3b17887ba382c72808a3;
+        LgObject = "<null>";
+        SystemAction =         {
+            Reminder =             {
+                Condition = Time;
+                Time =                 {
+                    Uri = "entity://Timex3";
+                    Version = "1.0";
+                    tid = 0;
+                    type = Set;
+                    value = "XXXX-XX-XXT09";
+                };
+                Title =                 {
+                    ProfanityMasked = "\U5403\U65e9\U996d";
+                    Uri = "entity://TextData";
+                    Value = "\U5403\U65e9\U996d";
+                    Version = "2.0";
+                };
+                Uri = "entity://Reminder";
+                Version = "2.0";
+            };
+            Uri = "action://Reminder/Create";
+            Version = "2.0";
+        };
+        TraceId = EF9F7E33CC42490ABDB63BC19CFAD471;
+        Uri = "action://CuOutput";
+    };
+    command = sendAction;
+    handler = CortanaCommon;
+    id = 9ssdelf;
+}
