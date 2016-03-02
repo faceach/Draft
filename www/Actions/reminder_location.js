7896c92434b5f7a23f568f9507142d8d504a787e
XiaominLiu <xiaoml@microsoft.com>
2016-03-02 18:40:53 +0800

    proactive page and actions

--- www/Actions/reminder_location.js
+++ www/Actions/reminder_location.js
@@ -1,1 +1,111 @@
-"{\"Uri\":\"action://CuOutput\",\"SystemAction\":{\"Reminder\":{\"Condition\":\"LeavingLocation\",\"Title\":{\"Value\":\"带电脑\",\"ProfanityMasked\":\"带电脑\",\"Uri\":\"entity://TextData\",\"Version\":\"2.0\"},\"Location\":[{\"Name\":\"work\",\"Geo\":{\"Longitude\":\"120.7288\",\"Latitude\":\"31.2655\",\"Uri\":\"entity://GeoCoordinates\",\"Version\":\"1.0\"},\"Uri\":\"entity://Place\",\"Version\":\"1.0\"}],\"UnresolvedLocation\":{\"Value\":\"办公室\",\"ProfanityMasked\":\"办公室\",\"Uri\":\"entity://TextData\",\"Version\":\"2.0\"},\"Uri\":\"entity://Reminder\",\"Version\":\"2.0\"},\"Uri\":\"action://Reminder/Create\",\"Version\":\"2.0\"},\"ConversationId\":null,\"TraceId\":\"D50E6AF7831249DA95FCB416D364D77B\",\"ImpressionId\":\"4664e93617274d2dd8436d8d447da342\",\"LgObject\":null}"
+"{\"Reminder\":{\"Condition\":\"LeavingLocation\",\"Title\":{\"Value\":\"办公室\",\"ProfanityMasked\":\"办公室\",\"Uri\":\"entity://TextData\",\"Version\":\"2.0\"},\"Location\":[{\"Name\":\"work\",\"Geo\":{\"Longitude\":\"120.7288\",\"Latitude\":\"31.2655\",\"Uri\":\"entity://GeoCoordinates\",\"Version\":\"1.0\"},\"Uri\":\"entity://Place\",\"Version\":\"1.0\"}],\"UnresolvedLocation\":{\"Value\":\"办公室\",\"ProfanityMasked\":\"办公室\",\"Uri\":\"entity://TextData\",\"Version\":\"2.0\"},\"Uri\":\"entity://Reminder\",\"Version\":\"2.0\"},\"Uri\":\"action://Reminder/Create\",\"Version\":\"2.0\"}"
+
+
+{
+	"Reminder": {
+		"Condition": "LeavingLocation",
+		"Title": {
+			"Value": "带电脑",
+			"ProfanityMasked": "带电脑",
+			"Uri": "entity:\/\/TextData",
+			"Version": "2.0"
+		},
+		"Location": [{
+			"Name": "work",
+			"Geo": {
+				"Longitude": "120.7288",
+				"Latitude": "31.2655",
+				"Uri": "entity:\/\/GeoCoordinates",
+				"Version": "1.0"
+			},
+			"Uri": "entity:\/\/Place",
+			"Version": "1.0"
+		}],
+		"UnresolvedLocation": {
+			"Value": "办公室",
+			"ProfanityMasked": "办公室",
+			"Uri": "entity:\/\/TextData",
+			"Version": "2.0"
+		},
+		"Uri": "entity:\/\/Reminder",
+		"Version": "2.0"
+	},
+	"Uri": "action:\/\/Reminder\/Create",
+	"Version": "2.0"
+}
+
+message = {LocationReminderMessage@6630} 
+ _locationName = {String@6641} "办公室"
+ _locationReminderType = {BingReminderType$2@6642} "Location"
+ _selectedPlace = null
+ _suggestedPlaces = {ArrayList@6643}  size = 1
+ _transissionType = 2
+ _confirmed = false
+ _hasConditionInfo = true
+ _title = {String@6644} "带电脑"
+ shadow$_klass_ = {Class@6629} "class com.microsoft.bing.dss.handlers.LocationReminderMessage"
+ shadow$_monitor_ = -2107690420
+
+ reminder = {BingReminderLocation@6670} 
+ _reminderGeofence = {BingReminderGeofence@6672} 
+ _geofenceExpirationPeriod = -1
+ _geofenceTransitionType = 2
+ _attachment = null
+ _completedAt = null
+ _detail = {String@6623} ""
+ _id = {String@6673} "9da4f510-3166-458d-8c45-6d1bc0993dce"
+ _snoozedTime = 0
+ _status = {BingReminderStatus$2@6674} "Active"
+ _title = {String@6644} "带电脑"
+ _type = {BingReminderType$2@6642} "Location"
+ shadow$_klass_ = {Class@5477} "class com.microsoft.bing.dss.reminderslib.base.BingReminderLocation"
+ shadow$_monitor_ = -1243009738
+
+----------------------------------------------------------
+ message = {LocationReminderMessage@6155} 
+ _locationName = {String@6162} "办公室"
+ _locationReminderType = {BingReminderType$2@6163} "Location"
+ _selectedPlace = null
+ _suggestedPlaces = {ArrayList@6164}  size = 1
+ _transissionType = 2
+ _confirmed = false
+ _hasConditionInfo = true
+ _title = {String@6165} "带电脑"
+ shadow$_klass_ = {Class@6154} "class com.microsoft.bing.dss.handlers.LocationReminderMessage"
+ shadow$_monitor_ = -1360956306
+
+ message = {LocationReminderMessage@6345} 
+ _locationName = null
+ _locationReminderType = {BingReminderType$2@6353} "Location"
+ _selectedPlace = {BingPlace@6354} 
+ _suggestedPlaces = {ArrayList@6355}  size = 1
+ _transissionType = 2
+ _confirmed = false
+ _hasConditionInfo = true
+ _title = {String@6356} "带电脑"
+ shadow$_klass_ = {Class@6236} "class com.microsoft.bing.dss.handlers.LocationReminderMessage"
+ shadow$_monitor_ = -1972030283
+
+ locationMessage = {LocationReminderMessage@6125} 
+ _locationName = null
+ _locationReminderType = {BingReminderType$2@6140} "Location"
+ _selectedPlace = {BingPlace@6141} 
+ _suggestedPlaces = {ArrayList@6142}  size = 1
+ _transissionType = 2
+ _confirmed = false
+ _hasConditionInfo = true
+ _title = {String@6143} "带电脑"
+ shadow$_klass_ = {Class@5977} "class com.microsoft.bing.dss.handlers.LocationReminderMessage"
+ shadow$_monitor_ = -1912792626
+
+ locationMessage = {LocationReminderMessage@5893} 
+ _locationName = {String@5898} "办公室"
+ _locationReminderType = {BingReminderType$2@5899} "Location"
+ _selectedPlace = null
+ _suggestedPlaces = {ArrayList@5900}  size = 1
+ _transissionType = 2
+ _confirmed = false
+ _hasConditionInfo = true
+ _title = {String@5901} "带电脑"
+ shadow$_klass_ = {Class@5891} "class com.microsoft.bing.dss.handlers.LocationReminderMessage"
+ shadow$_monitor_ = -1206725390
