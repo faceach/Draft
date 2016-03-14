c8d3b2bad862c91285c2143df0a753fb07400e93
XiaominLiu <xiaoml@microsoft.com>
2016-03-14 19:28:53 +0800

    set timer action

---
+++ www/spa/settimer_action.js
@@ -0,0 +1,12 @@
+{
+	"Uri": "action:\/\/CuOutput",
+	"SystemAction": {
+		"QueryUri": "https:\/\/www.bing.com\/search?q=set+a+timer+for+20+minutes&input=1&nclid=E40B55FAEC1515C9A5D882D529DA6C15&FORM=",
+		"Uri": "action:\/\/Conversation\/ShowUrlContent",
+		"Version": "1.0"
+	},
+	"ConversationId": "cd3e98ac-3023-edfe-6812-36ce7405f1b3",
+	"TraceId": "E10E5F22535E4CF6A4C707FB90F92F58",
+	"ImpressionId": "bbf9251ad461ed1fae69e58ceb19adb0",
+	"LgObject": null
+}
