19bda10e6a3b9634ef15884703ae41c68e066bc3
Xiaomin Liu <xiaoml@microsoft.com>
2016-04-01 11:49:32 +0800

    cyanogen

--- www/proactive/cyanogen.js
+++ www/proactive/cyanogen.js
@@ -1,8 +1,8 @@
 ! function() {
 	var elStyle = document.createElement('style');
 	document.head.appendChild(elStyle);
-	elStyle.innerText = '#b_results>li.b_ans {border-radius: 10px;}' +
-		'' +
+	elStyle.innerText = '#b_results>li.b_ans { border-radius: 10px; }' +
+		'#b_results .b_ans+.b_ans { margin-top: 20px; }' +
 		'' +
 		'' +
 		'';
