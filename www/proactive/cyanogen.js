58d58dcd122c331e2f8604efd7981e1a4d4a3cc6
Xiaomin Liu <xiaoml@microsoft.com>
2016-04-05 12:15:51 +0800

    cyanogen

--- www/proactive/cyanogen.js
+++ www/proactive/cyanogen.js
@@ -1,9 +1,9 @@
 ! function() {
 	var elStyle = document.createElement('style');
 	document.head.appendChild(elStyle);
-	elStyle.innerText = '#b_results>li.b_ans { border-radius: 10px; overflow: hidden; }' +
-		'#b_results .b_ans+.b_ans { margin-top: 24px; }' +
-		'#b_results>li.b_ans .b_ansSlice:last-child .miniExpanded .miniExpIcon { border-bottom-right-radius: 10px; border-bottom-left-radius: 10px; }' +
+	elStyle.innerText = '#b_results > li.b_ans { border-radius: 10px; }' +
+		'#b_results > li.b_ans + .b_ans { margin-top: 24px; }' +
+		'#b_results > li.b_ans .b_ansSlice:last-child .miniExpanded .miniExpIcon { border-bottom-right-radius: 10px; border-bottom-left-radius: 10px; }' +
 		'' +
 		'';
 }();
