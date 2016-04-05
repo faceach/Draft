72778d6d9c2f4c24bad82fcb247c29d3a3c17a0d
Xiaomin Liu <xiaoml@microsoft.com>
2016-04-05 18:03:24 +0800

    cyanogen

--- www/proactive/cyanogen.js
+++ www/proactive/cyanogen.js
@@ -4,7 +4,8 @@
 	elStyle.innerText = '#b_results > li.b_ans { border-radius: 10px; }' +
 		'#b_results > li.b_ans + .b_ans { margin-top: 24px; }' +
 		'#b_results > li.b_ans .b_ansSlice:last-child .miniExpanded .miniExpIcon { border-bottom-right-radius: 10px; border-bottom-left-radius: 10px; }' +
-		'#b_results > li.b_ans > .actDrawer .actContainer, #b_results > li.b_ans .b_ansSlice:first-child .actDrawer .actContainer { border-top-left-radius: 10px; border-top-right-radius: 10px; }' +
-		'#b_results > li.b_ans .b_ansSlice { background-color: red; }' +
+		'#b_results > li.b_ans > .actDrawer .actContainer { border-top-left-radius: 10px; border-top-right-radius: 10px; }' +
+		'#b_results > li.b_ans .b_ansSlice .actDrawer .actContainer { border-top-left-radius: 10px; border-top-right-radius: 10px; }' +
+		'#b_results > li.b_ans .b_ansSlice ~ .b_ansSlice .actDrawer .actContainer { border-top-left-radius: 0; border-top-right-radius: 0; }' +
 		'';
 }();
