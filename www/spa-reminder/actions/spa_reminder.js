1758a2bc4247b05ef65697d2677b26574b98d651
xiaoml <xiaoml@microsoft.com>
2016-05-20 19:25:44 +0800

    spa reminder

--- www/spa-reminder/actions/spa_reminder.js
+++ www/spa-reminder/actions/spa_reminder.js
@@ -40,3 +40,7 @@
 	"Uri": "action://Reminder/Create",
 	"Version": "2.0"
 }
+
+{"Reminder":{"Condition":"Time","Title":{"Value":"吃早餐","ProfanityMasked":"吃早餐","Uri":"entity:\/\/TextData","Version":"2.0"},"Time":{"tid":"0","type":"Time","value":"2016-05-21TMO","Uri":"entity:\/\/Timex3","Version":"1.0"},"Uri":"entity:\/\/Reminder","Version":"2.0"},"Uri":"action:\/\/Reminder\/Create","Version":"2.0"}
+
+Bundle[{dialogAction={"Reminder":{"Condition":"Time","Title":{"Value":"吃早餐","ProfanityMasked":"吃早餐","Uri":"entity:\/\/TextData","Version":"2.0"},"Time":{"tid":"0","type":"Time","value":"2016-05-21TMO","Uri":"entity:\/\/Timex3","Version":"1.0"},"Uri":"entity:\/\/Reminder","Version":"2.0"},"Uri":"action:\/\/Reminder\/Create","Version":"2.0"}, url=action://Reminder/Create, context=action://Reminder/Create}]
