33c501f4fc26bd1c5a9b3cd7700467bd38cf39b5
xiaoml <xiaoml@microsoft.com>
2016-06-23 12:19:37 +0800

    home screen

--- www/newhome/script.js
+++ www/newhome/script.js
@@ -5,66 +5,90 @@ document.getElementById('action_container').style.position = 'static';
 document.getElementById('proactive_peek_overlay').remove();
 document.getElementById('chevron_bar').style.position = 'static';
 
-function swipedetect(el, callback) {
-	var touchsurface = el,
-		swipedir,
-		startX,
-		startY,
-		distX,
-		distY,
-		threshold = 5, //required min distance traveled to be considered swipe
-		restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-		allowedTime = 300, // maximum time allowed to travel that distance
-		elapsedTime,
-		startTime,
-		handleswipe = callback || function(swipedir) {};
+(function() {
+	var isScrollEnabled = false;
+	var peekHeight = 0;
+	var elPeekMain = document.getElementById('proactive_peek_main');
 
-	touchsurface.addEventListener('touchstart', function(e) {
-		var touchobj = e.changedTouches[0];
-		swipedir = 'none';
-		startX = touchobj.pageX;
-		startY = touchobj.pageY;
-		startTime = new Date().getTime(); // record time when finger first makes contact with surface
-		//e.preventDefault()
-	}, false)
+	function swipedetect(el, callback) {
+		var touchsurface = el,
+			swipedir,
+			startX,
+			startY,
+			distX,
+			distY,
+			threshold = 5, //required min distance traveled to be considered swipe
+			restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+			allowedTime = 800, // maximum time allowed to travel that distance
+			elapsedTime,
+			startTime,
+			handleswipe = callback || function(swipedir) {};
 
-	touchsurface.addEventListener('touchmove', function(e) {
-		//e.preventDefault() // prevent scrolling when inside DIV
-	}, false)
+		touchsurface.addEventListener('touchstart', function(e) {
+			var touchobj = e.changedTouches[0];
+			swipedir = 'none';
+			startX = touchobj.pageX;
+			startY = touchobj.pageY;
+			startTime = new Date().getTime(); // record time when finger first makes contact with surface
+			//e.preventDefault()
+		}, false)
 
-	touchsurface.addEventListener('touchend', function(e) {
-		var touchobj = e.changedTouches[0];
-		distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
-		distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
-		elapsedTime = new Date().getTime() - startTime; // get time elapsed
-		if (elapsedTime <= allowedTime) { // first condition for awipe met
-			console.log('X:' + distX);
-			console.log('Y:' + distY);
-			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
-				swipedir = (distX < 0) ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
-			} else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
-				swipedir = (distY < 0) ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
+		touchsurface.addEventListener('touchmove', function(e) {
+			if (!isScrollEnabled) {
+				e.preventDefault() // prevent scrolling when inside DIVP
+			}
+		}, false)
+
+		touchsurface.addEventListener('touchend', function(e) {
+			var touchobj = e.changedTouches[0];
+			distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+			distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+			elapsedTime = new Date().getTime() - startTime; // get time elapsed
+			console.log('elapsedTime:' + elapsedTime);
+			if (elapsedTime <= allowedTime) { // first condition for awipe met
+				console.log('X:' + distX);
+				console.log('Y:' + distY);
+				if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+					swipedir = (distY < 0) ? 'up' : 'down'; // if dist traveled is negative, it indicates up swipe
+				} else if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+					swipedir = (distX < 0) ? 'left' : 'right'; // if dist traveled is negative, it indicates left swipe
+				}
+			}
+			handleswipe(swipedir);
+			//e.preventDefault()
+		}, false);
+	}
+
+	function hidePeek() {
+		if (!peekHeight) {
+			peekHeight = elPeekMain.clientHeight;
+		}
+		elPeekMain.style.marginTop = (-1 * peekHeight) + 'px';
+
+		setTimeout(function() {
+			isScrollEnabled = true;
+		}, 400);
+	}
+
+	function showPeek() {
+		elPeekMain.style.marginTop = 0;
+		isScrollEnabled = false;
+	}
+
+	swipedetect(document.documentElement, function(swipedir) {
+		console.log('You just swiped: ' + swipedir);
+		if (swipedir.toLowerCase() === 'up') {
+			hidePeek();
+		} else if (swipedir.toLowerCase() === 'down') {
+			if (document.body.scrollTop < peekHeight) {
+				showPeek();
 			}
 		}
-		console.log(swipedir);
-		handleswipe(swipedir);
-		//e.preventDefault()
-	}, false);
-}
+	});
 
-function hidePeek() {
-	var peekHeight = document.getElementById('proactive_peek_main').clientHeight;
-	document.getElementById('proactive_peek_main').style.marginTop = (-1 * peekHeight) + 'px';
-}
+	document.getElementById('chevron_bar').addEventListener('click', function(e) {
+		e.preventDefault();
+		hidePeek();
+	});
 
-swipedetect(document.documentElement, function(swipedir) {
-	if (swipedir.toLowerCase() === 'up') {
-		console.log('You just swiped left!');
-		//hidePeek();
-	}
-});
-
-document.getElementById('chevron_bar').addEventListener('click', function(e) {
-	e.preventDefault();
-	hidePeek();
-});
+})();
